84b943a223c5c8d04b52bc90751d7659c5126ce7
TrueDi1905 <75692134+TrueDi1905@users.noreply.github.com>
2021-10-14 12:31:12 +0300

    'v1'

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+
 from users.serializers import CustomUserSerializer
 
 from .fields import Base64ImageField
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
+
 from users.views import UserPagination
 
 from .filters import RecipeFilter
