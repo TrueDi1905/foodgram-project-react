6b034d1115e36bc0791659bd1be83c61abe5cf00
TrueDi1905 <75692134+TrueDi1905@users.noreply.github.com>
2021-10-14 23:17:18 +0300

    'v1'

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from users.serializers import CustomUserSerializer
-
 from .fields import Base64ImageField
 from .models import (FavoriteRecipe, Ingredient, IngredientAmount, Recipe,
                      ShoppingCart, Tag, User)
