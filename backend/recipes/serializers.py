3253d712f55af8542266ba335d36c9b3d06cbc8a
TrueDi1905 <75692134+TrueDi1905@users.noreply.github.com>
2021-10-18 10:59:56 +0300

    'v1'

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -98,11 +98,6 @@ class RecipeSerializer(serializers.ModelSerializer):
             tags_set.add(tag)
         return data
 
-    def validate_cooking_time(self, data):
-        if data <= 0:
-            raise serializers.ValidationError(
-                'Время не должно быть 0 или меньше!')
-        return data
 
     def ingredient_add(self, ingredients, recipe):
         for ingredient in ingredients:
